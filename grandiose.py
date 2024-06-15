e32c73ac4f2628bd6f1e014114d6f2f4c9bf3d58
Sam Crowe <crowesam@users.noreply.github.com>
2024-06-15 01:39:58 -0400

    Update grandiose.py
    For some reason I had to copy and paste this manually even though I tried the commit and Commit+Push from within PsyCharm

---
+++ grandiose.py
@@ -0,0 +1,5 @@
+#Sam Crowe
+#get synonym for faster from user
+
+faster_like=input("PLease enter a synonym for the word 'faster: ")
+print("My laptop is so much " + faster_like + " than yours I am surprised you don't call it a latetop! Hahaha!")
