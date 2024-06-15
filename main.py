2eb4a5756e7089320e984a435bfb8f7c0966bf08
Sam Crowe <crowesam@users.noreply.github.com>
2024-06-15 01:18:23 -0400

    Here we go adding this to my GitHub account for the first time even though I have done this twice!

---
+++ main.py
@@ -0,0 +1,8 @@
+#Sam Crowe
+#1 get movie rating from user
+
+# This one can use eval(), int(), or even the float() functions
+rating = int(input("Can you give a rating of your favorite moview, on a scale of 1-5? "))
+mine = 5
+print(mine-rating)
+
